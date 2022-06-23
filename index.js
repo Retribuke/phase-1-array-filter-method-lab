910a3ee02ecbd78e177a26390f88bc31a3c13972
Justyn Enyart <gweeners17@gmail.com>
2022-06-23 18:09:12 -0400

    commit tree 6a99373da055b6dc370fc25b5c267611c6b5137d

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+const findMatching = (array, string) => {
+    return array.filter(newDrivers => newDrivers.toLowerCase() === string.toLowerCase())
+}
+
+const fuzzyMatch = (array, string) => {
+    return array.filter(firstLetter => firstLetter.startsWith(string));
+}
+
+const matchName = (array, string) => {
+    return array.filter(arrayName => arrayName.name === string);
+}
